001b8be9b69ce10ac81c273c83b8fed9674c064d
Patrick <patrickwigley2@gmail.com>
2023-08-11 15:07:09 +0100

    Implemented some key typing functionality

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
 	"github.com/hajimehoshi/ebiten/v2/vector"
 )
 
@@ -12,23 +13,43 @@ const WORDS_LETTERS_COUNT = 5
 const CELL_SIZE = 100
 
 type Game struct {
+	keys []ebiten.Key
 }
 
 type Cell struct {
-	character string
-	position  int32
+	character    string
+	position     int32
+	string_typed string
+	is_empty     bool
 }
 type Word struct {
 	cells [WORDS_LETTERS_COUNT]Cell
 }
 
 var (
-	width         = 1300
-	height        = 800
-	word_to_guess = Word{}
+	width          = 1300
+	height         = 800
+	word_to_guess  = ""
+	word           = Word{}
+	typed_word_str = ""
 )
 
 func (g *Game) Update() error {
+	g.keys = inpututil.AppendJustReleasedKeys(g.keys[:0])
+
+	for _, val := range g.keys {
+		char := ebiten.KeyName(val)
+
+		typed_word_str += char
+		for _, cell := range word.cells {
+			if cell.is_empty {
+				cell.character = char
+				break
+			}
+		}
+	}
+
+	println(typed_word_str)
 
 	return nil
 }
